9dd4984d4cec685efd53451ad43718d33ff284c0
thuanhphan <phananhthu9333@gmail.com>
2018-11-19 17:05:10 +0700

    Get values from signup form

--- tour_of_heroes_ui/src/app/sign-up/sign-up.component.ts
+++ tour_of_heroes_ui/src/app/sign-up/sign-up.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -10,18 +10,33 @@ export class SignUpComponent implements OnInit {
 
   signUpForm: FormGroup;
 
-  constructor(
-    private fb: FormBuilder
-  ) { }
+  constructor(private fb: FormBuilder) {
+    this.signUpForm = fb.group({
+      'userName' : [],
+      'password' : [],
+      'passwordRepeat': [],
+      'email' : [],
+      'firstName': [],
+      'lastName': []
 
-  ngOnInit() {
-    this.signUpForm = this.fb.group({
-      userName: []
     });
   }
 
-  signUp() {
-    console.log(this.signUpForm.value);
+  ngOnInit() {
+
+
   }
+
+  addPost() {
+    console.log(this.signUpForm.value);
+    const userName = this.signUpForm.value.userName;
+    const psw = this.signUpForm.value.password;
+    const pswRepeat = this.signUpForm.value.passwordRepeat;
+    const email = this.signUpForm.value.email;
+    const firstName = this.signUpForm.value.firstName;
+    const lastName = this.signUpForm.value.lastName;
+  }
+
+
 
 }
